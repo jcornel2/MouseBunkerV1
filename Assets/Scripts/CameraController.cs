fe7e3baa5b8d4dfa23ce8ebe22cc8cc4c4aa9a35
jcornel2 <66965271+jcornel2@users.noreply.github.com>
2021-08-29 14:41:44 +0200

    Added the world and basic player movement

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Transform target;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = target.position + new Vector3(0.0f, 0.0f, -10);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Vector2 speed = new Vector2(50, 50);
+    public Rigidbody2D rb2d;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float inputX = Input.GetAxis("Horizontal");
+
+        Vector2 vel = rb2d.velocity;
+        vel.x = speed.x * inputX;
+
+        //vel *= Time.deltaTime;
+
+        rb2d.velocity = vel;
+
+        //transform.Translate(movement);
+    }
+}
